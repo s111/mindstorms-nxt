dd81c39bd767ffe5696c89bbc810378d32ce59cd
Sebastian <Sebastian@Sebastian-PC>
2012-11-12 17:07:01 +0100

    Cleaned up the code

--- NXTDisconnect.m
+++
@@ -1,6 +0,0 @@
-function NXTDisconnect()
-%NXTDISCONNECT
-%   Close connection to the NXT
-
-        COM_CloseNXT(COM_GetDefaultNXT());
-end
--- nxtCleanup.m
+++
@@ -1,8 +0,0 @@
-%NXTCLEANUP script
-%   Disconnect from the NXT and release joystick
-
-% Disconnect from the NXT
-NXTDisconnect();
-
-% Release joystick
-clear('joymex2');
